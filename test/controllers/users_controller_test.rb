7cc2bc7dab00b7d09f894626d8e12c82ade8bab8
Kensei itatani <kensei.itatani@mulodo.com>
2017-12-13 15:24:15 +0700

    Delete User #8

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,28 +3,30 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:michael)
-    @other_user = users(:archer)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "should redirect edit when not logged in" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
+ test "index as admin includeing pagination and delete links" do
+  log_in_as(@admin)
+  get users_path
+  assert_template 'users/index'
+  assert_select 'div.pagination'
+  first_page_of_users = User.paginate(page: 1)
+  first_page_of_users.each do |user|
+    assert_select 'a[href=?]', user_path(user), text: user.name
+    unless user == @admin
+      assert_select 'a[href=?]', user_path(user), text: "delete"
+    end
   end
-  
-  test "should redirect update when not logged in" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: {user: { name: @user.name,
-                              email: @user.email }}
-    assert flash.empty?
-    assert_redirected_to root_url
-  end       
-  
-  test "should redirect index when not logged in" do
+  assert_difference "User.count", -1 do
+    delete user_path(@non_admin)
+  end
+end
+
+test "index as non-admin" do
+    log_in_as(@non_admin)
     get users_path
-    assert_redirected_to login_url
+    assert_select 'a', text: 'delete', count: 0
   end
-
 end
