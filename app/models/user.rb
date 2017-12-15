d9a6b3955ca5dae61f826f5cd6726837e9f482eb
Kensei itatani <kensei.itatani@mulodo.com>
2017-12-15 17:07:11 +0700

    Add user following #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id) #idはself.idである?のおかげでエスケープされるから使えるのか？
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+        OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
